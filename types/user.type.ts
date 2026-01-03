c00e2a7e134a232cd1bbf5089663406ed3b43f39
Wafiqsw <wafiqmisman06@gmail.com>
2026-01-03 19:53:07 +0800

    fix deploy

--- types/user.type.ts
+++ types/user.type.ts
@@ -2,6 +2,7 @@ export type User = {
     id: string;
     email: string;
     password?: string; // Temporary password (deleted when user changes password)
+    tempPasswordStatus?: boolean; // Whether user is using temporary password
     firstName?: string;
     lastName?: string;
     phoneNumber?: string;
